e5d7be6c6efceec548a4ba59ac9a217c1b88bbd4
Ben Zikri <57237262+ZikriBen@users.noreply.github.com>
2023-07-16 20:41:15 +0300

    Update RoboBrain.h

--- Headers/RoboBrain.h
+++ Headers/RoboBrain.h
@@ -9,62 +9,43 @@
 #include <queue>
 #include <thread>
 #include <vector>
-
 #include <map>
 #include <condition_variable>
+#include <functional>
+
 #include <yaml-cpp/node/node.h>
 #include <yaml-cpp/parser.h>
 #include <yaml-cpp/yaml.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
-class NetFunction {
-public:
-    virtual void execute(std::vector<double> &motorPositions) = 0;
-};
-
-class NetFunctionReverse : public NetFunction {
-public:
-    void execute(std::vector<double> &motorPositions) override;
-};
-
-class NetFunctionToBase : public NetFunction {
-public:
-    void execute(std::vector<double> &motorPositions) override;
-};
-
-class NetFunctionFactory {
-public:
-    void registerFunction(const std::string& funcName, std::function<NetFunction* ()> funcPtr);
-    NetFunction* createFunction(const std::string& funcName);
-    bool funcExists(const std::string& funcName);
-private:
-    std::map<std::string, std::function<NetFunction* ()>> m_functionMap;
-};
-constexpr double FULLCIRCLE = 360.0;
-
-struct Configuration {
-    int motorCount;
-    std::vector<std::string> netFunctionNames;
-};
-
-
+//RoboBrain
 class RoboBrain {
 private:
     Configuration m_Config;
+    NetFunctionFactory m_factory;
+    
     bool m_isConfigured;
     bool m_isRunning;
+    bool m_currentNetFunction;
+    long m_timer;
+
     std::vector<double> m_MotorPositions;
+    std::queue<std::string> m_funcQ;
     std::mutex m_Mutex;
     std::mutex m_funcMutex;
-    std::queue<std::string> m_funcQ;
-    bool m_currentNetFunction;
+    
     std::thread m_mainLoopThread;
     std::thread m_motorsLoopThread;
     std::thread m_positionLoopThread;
-    long m_timer;
+    std::thread m_timerLoopThread;
+    
     std::condition_variable m_motorsLoopCondition;
     std::condition_variable m_positionLoopCondition;
-    NetFunctionFactory m_factory;
-
+    std::condition_variable m_mainLoopCondition;
+    
+    
+    void timerLoop();
     void mainLoop();
     void motorsLoop();
     void positionLoop();
@@ -78,4 +59,37 @@ public:
     void addNetFunc(std::string funcName);
     void run();
     void stop();
+};
+
+// NetFunction
+class NetFunction {
+public:
+    virtual void execute(std::vector<double>& motorPositions) = 0;
+};
+
+class NetFunctionReverse : public NetFunction {
+public:
+    void execute(std::vector<double>& motorPositions) override;
+};
+
+class NetFunctionToBase : public NetFunction {
+public:
+    void execute(std::vector<double>& motorPositions) override;
+};
+
+class NetFunctionFactory {
+public:
+    void registerFunction(const std::string& funcName, std::function<NetFunction* ()> funcPtr);
+    NetFunction* createFunction(const std::string& funcName);
+    bool funcExists(const std::string& funcName);
+private:
+    std::map<std::string, std::function<NetFunction* ()>> m_functionMap;
+};
+constexpr double FULLCIRCLE = 360.0;
+
+
+// Configuration
+struct Configuration {
+    int motorCount;
+    std::vector<std::string> netFunctionNames;
 };
